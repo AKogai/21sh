9bd511e26f5d78b662a65e4035d2bc106cae7117
kosimovsky <git.kosimovsky@gmail.com>
2020-06-27 12:29:15 +0300

    script updated

--- scripts/add_h.sh
+++ scripts/add_h.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 
-sed -i '1s/^/# **************************************************************************** #\n/' Makefile
-sed -i '2s/^/#                                                                              #\n/' Makefile
-sed -i '3s/^/#                                                         :::      ::::::::    #\n/' Makefile
-sed -i '4s/^/#    Makefile                                           :+:      :+:    :+:    #\n/' Makefile
-sed -i '5s/^/#                                                     +:+ +:+         +:+      #\n/' Makefile
-sed -i '6s/^/#    By: cchieko <marvin@42.fr>                     +#+  +:+       +#+         #\n/' Makefile
-sed -i '7s/^/#                                                 +#+#+#+#+#+   +#+            #\n/' Makefile
-sed -i '8s/^/#    Created: 2020\/06\/06 20:35:56 by cchieko           #+#    #+#              #\n/' Makefile
-sed -i '9s/^/#    Updated: 2020\/06\/07 15:51:29 by cchieko          ###   ########.fr        #\n/' Makefile
-sed -i '10s/^/#                                                                              #\n/' Makefile
-sed -i '11s/^/# **************************************************************************** #\n/' Makefile
+sed -i '' -E '1s/^/# **************************************************************************** #\\\n/' Makefile
+sed -i '' -E '2s/^/#                                                                              #\\\n/' Makefile
+sed -i '' -E '3s/^/#                                                         :::      ::::::::    #\\\n/' Makefile
+sed -i '' -E '4s/^/#    Makefile                                           :+:      :+:    :+:    #\\\n/' Makefile
+sed -i '' -E '5s/^/#                                                     +:+ +:+         +:+      #\\\n/' Makefile
+sed -i '' -E '6s/^/#    By: cchieko <marvin@42.fr>                     +#+  +:+       +#+         #\\\n/' Makefile
+sed -i '' -E '7s/^/#                                                 +#+#+#+#+#+   +#+            #\\\n/' Makefile
+sed -i '' -E '8s/^/#    Created: 2020\/06\/06 20:35:56 by cchieko           #+#    #+#              #\\\n/' Makefile
+sed -i '' -E '9s/^/#    Updated: 2020\/06\/07 15:51:29 by cchieko          ###   ########.fr        #\\\n/' Makefile
+sed -i '' -E '10s/^/#                                                                              #\\\n/' Makefile
+sed -i '' -E '11s/^/# **************************************************************************** #\\\n/' Makefile
 
-sed -i 's/FLAGS = -Wall -Wextra -Werror -g/FLAGS = -Wall -Wextra -Werror/' Makefile
+sed -i '' -E 's/FLAGS = -Wall -Wextra -Werror -g/FLAGS = -Wall -Wextra -Werror/' Makefile
 
 exit 0
